11de98d2be4a9e63f6dbc610db3f9aac997c0ef9
Brij Kishor <exreply7878@gmail.com>
2022-07-01 16:41:34 +0530

    improved UI

--- source/main.js
+++ source/main.js
@@ -1,5 +1,5 @@
-const video = document.querySelector('.item1 video')
-const img = document.querySelector('.item2 img')
+const video = document.querySelector('video')
+const img = document.querySelector('.image')
 const startWebCamBtn = document.querySelector('.start-webcam-btn')
 const captureBtn = document.querySelector('.disabled-btn')
 
@@ -12,6 +12,7 @@ function webCam(){
        navigator.mediaDevices.getUserMedia({video: true})
        .then( (stream)=>{
            video.srcObject = stream
+           console.log(video.srcObject)
            captureBtn.classList.remove('disabled-btn')
        })
     }
@@ -23,8 +24,6 @@ function takeSnap(){
     //  Canvas
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
-    canvas.style.height = video.style.height
-    canvas.style.width = video.style.width
     
     // drawing image to canvas
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
@@ -33,6 +32,7 @@ function takeSnap(){
     const dataURI = canvas.toDataURL("image/png")
 
     // setting image to img tag
-    img.src = dataURI
+ img.src = dataURI
+    
     
 }
